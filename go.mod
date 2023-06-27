d025c1d338abae3c07207de8c862290ee5e09365
suainul2 <suainulovi@gmail.com>
2023-06-27 16:37:37 +0700

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gojob
+
+go 1.19
---
+++ gojob.go
@@ -0,0 +1,64 @@
+package gojob
+
+import (
+	"sync"
+	"time"
+)
+
+type Job struct {
+	ID        string
+	Data      string
+	Delay     int
+	Fun       func(Job)
+	CreatedAt time.Time
+}
+
+type JobQueue struct {
+	Jobs     chan Job
+	StopChan chan string
+	// tambahkan field lain yang diperlukan
+}
+
+var blacklist []string
+
+func NewGojob() *JobQueue {
+	return &JobQueue{
+		Jobs:     make(chan Job),
+		StopChan: make(chan string),
+	}
+}
+
+func (jq *JobQueue) AddJob(job Job) {
+	job.CreatedAt = time.Now()
+	jq.Jobs <- job
+}
+
+func (jq *JobQueue) Stop(uid string) {
+	blacklist = append(blacklist, uid)
+}
+func searchBlacklist(search string) bool {
+	for _, x := range blacklist {
+		if x == search {
+			return false
+		}
+	}
+	return true
+}
+func (jq *JobQueue) ProcessJobs() {
+	var wg sync.WaitGroup
+
+	for {
+		select {
+		case job := <-jq.Jobs:
+			wg.Add(1)
+			go func(job Job) {
+				defer wg.Done()
+				time.Sleep(time.Duration(job.Delay) * time.Second) // contoh: tunggu 1 detik untuk memproses job
+				// Proses job di sini
+				if searchBlacklist(job.ID) {
+					job.Fun(job)
+				}
+			}(job)
+		}
+	}
+}
